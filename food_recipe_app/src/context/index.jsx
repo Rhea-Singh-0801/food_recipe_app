6d6d2ccf44f2152d759c8e8d898cb150cc43bba5
Rhea-Singh-0801 <rheas804@gmail.com>
2024-10-12 15:51:01 +0100

    Favorite item functionality added

--- food_recipe_app/src/context/index.jsx
+++ food_recipe_app/src/context/index.jsx
@@ -8,6 +8,7 @@ export default function GlobalState({ children }) {
   const [loading, setLoading] = useState(false);
   const [recipeList, setRecipeList] = useState([]);
   const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favoritesList, setFavoritesList] = useState([]);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -32,6 +33,24 @@ export default function GlobalState({ children }) {
 
   //   console.log(loading, recipeList);
 
+  function handleAddToFavorite(getCurrentItem) {
+    console.log(getCurrentItem);
+    let cpyFavoritesList = [...favoritesList];
+    const index = cpyFavoritesList.findIndex(
+      (item) => item.id === getCurrentItem.id
+    );
+
+    if (index === -1) {
+      cpyFavoritesList.push(getCurrentItem);
+    } else {
+      cpyFavoritesList.splice(index);
+    }
+
+    setFavoritesList(cpyFavoritesList);
+  }
+
+  console.log(favoritesList, "favoritesList");
+
   return (
     <GlobalContext.Provider
       value={{
@@ -42,6 +61,8 @@ export default function GlobalState({ children }) {
         handleSubmit,
         recipeDetailsData,
         setRecipeDetailsData,
+        handleAddToFavorite,
+        favoritesList,
       }}
     >
       {children}
